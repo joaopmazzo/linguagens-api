dc537a5cad407f6f6cbd2c96517b7eb3bcc5d581
João Paulo Mazzo <mazzojp@gmail.com>
2022-07-23 15:07:34 -0300

    update: implement new GetMapping that get all data in our mongoDB collection

--- src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
+++ src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
@@ -1,5 +1,23 @@
 package br.com.joaopmazzo.linguagens.api.controller;
 
+import br.com.joaopmazzo.linguagens.api.connection.LinguagemRepository;
+import br.com.joaopmazzo.linguagens.api.linguagemDAO.Linguagem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class LinguagemController {
+
+    @Autowired
+    private LinguagemRepository repository;
+
+    @GetMapping(value = "/linguagens")
+    public List<Linguagem> retornaLinguagens() {
+        List<Linguagem> linguagens = repository.findAll();
+        return linguagens;
+    }
 
 }

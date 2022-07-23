489702b295a99cf6576fefa2f5dd32533e92be9c
João Paulo Mazzo <mazzojp@gmail.com>
2022-07-23 15:05:27 -0300

    feat: create the connections with our database in mongoDB

--- src/main/java/br/com/joaopmazzo/linguagens/api/connection/LinguagemRepository.java
+++ src/main/java/br/com/joaopmazzo/linguagens/api/connection/LinguagemRepository.java
@@ -1,4 +1,8 @@
 package br.com.joaopmazzo.linguagens.api.connection;
 
-public interface LinguagemRepository {
+import br.com.joaopmazzo.linguagens.api.linguagemDAO.Linguagem;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+
 }

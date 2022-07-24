5aa29a13c43c2b9b9759073c9424ff23cbd5b612
João Paulo Mazzo <mazzojp@gmail.com>
2022-07-23 22:34:09 -0300

    update: add PostMapping

--- src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
+++ src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
@@ -4,6 +4,8 @@ import br.com.joaopmazzo.linguagens.api.connection.LinguagemRepository;
 import br.com.joaopmazzo.linguagens.api.linguagemDAO.Linguagem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +20,12 @@ public class LinguagemController {
     public List<Linguagem> retornaLinguagens() {
         List<Linguagem> linguagens = repository.findAll();
         return linguagens;
+    }
+
+    @PostMapping(value = "/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva = repository.save(linguagem);
+        return linguagemSalva;
     }
 
 }

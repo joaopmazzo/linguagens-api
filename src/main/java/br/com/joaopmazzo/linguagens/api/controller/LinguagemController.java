8d75fc2443ff40f1235e145b989b3c37f333f29e
João Paulo Mazzo <mazzojp@gmail.com>
2022-07-24 16:02:52 -0300

    feat: implement DeleteMapping, PutMapping, and refactor other existing methods.

--- src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
+++ src/main/java/br/com/joaopmazzo/linguagens/api/controller/LinguagemController.java
@@ -1,14 +1,14 @@
 package br.com.joaopmazzo.linguagens.api.controller;
 
-import br.com.joaopmazzo.linguagens.api.connection.LinguagemRepository;
-import br.com.joaopmazzo.linguagens.api.linguagemDAO.Linguagem;
+import br.com.joaopmazzo.linguagens.api.repository.LinguagemRepository;
+import br.com.joaopmazzo.linguagens.api.entity.Linguagem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LinguagemController {
@@ -17,15 +17,59 @@ public class LinguagemController {
     private LinguagemRepository repository;
 
     @GetMapping(value = "/linguagens")
-    public List<Linguagem> retornaLinguagens() {
-        List<Linguagem> linguagens = repository.findAll();
-        return linguagens;
+    public ResponseEntity<List<Linguagem>> retornaLinguagens() {
+        try {
+            List<Linguagem> linguagens = repository.findAll();
+            return new ResponseEntity<>(linguagens, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/linguagens/{id}")
+    public ResponseEntity<Linguagem> retornaLinguagemId(@PathVariable(value = "id") String id) {
+        Optional<Linguagem> linguagem = repository.findById(id);
+        if (linguagem.isPresent()) {
+            return new ResponseEntity<>(linguagem.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping(value = "/linguagens")
-    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
-        Linguagem linguagemSalva = repository.save(linguagem);
-        return linguagemSalva;
+    public ResponseEntity<Linguagem> cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        try {
+            Linguagem linguagemSalva = repository.save(linguagem);
+            return new ResponseEntity<>(linguagemSalva, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
+    @PutMapping(value = "/linguagens/{id}")
+    public ResponseEntity<Linguagem> atualizarLinguagem(@PathVariable(value = "id") String id, @RequestBody Linguagem linguagem) {
+        Optional<Linguagem> linguagemParaAtualizada = repository.findById(id);
+
+        if (linguagemParaAtualizada.isPresent()) {
+            Linguagem _linguagem = linguagemParaAtualizada.get();
+            _linguagem.setTitle(linguagem.getTitle());
+            _linguagem.setImage(linguagem.getImage());
+            _linguagem.setRanking(linguagem.getRanking());
+
+            Linguagem linguagemAtualizada = repository.save(_linguagem);
+            return new ResponseEntity<>(linguagemAtualizada, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/linguagens/{id}")
+    public ResponseEntity<HttpStatus> excluiLinguagemPorId(@PathVariable(value = "id") String id) {
+        try {
+            repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
